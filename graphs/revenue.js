122dc5681424aa85de52f4896ff36a5d1f0790b7
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-08-31 11:19:30 +0530

    feat: add revenue bar chart

---
+++ graphs/revenue.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const revenueLink = document.querySelector('.sidebar nav ul li:nth-child(2) a');
+    const fileInput = document.getElementById('add-dataset-upload');
+    const dashboard = document.querySelector('main.dashboard');
+    const fileNameSpan = document.getElementById('file-name');
+
+    let selectedFile = null;
+    fileInput.addEventListener('change', (e) => {
+        selectedFile = e.target.files[0];
+        fileNameSpan.textContent = selectedFile ? selectedFile.name : '';
+    });
+
+    revenueLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!selectedFile) {
+            alert('Please add dataset file first');
+            return;
+
+        }
+
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            let data;
+            if (selectedFile.name.endsWith('.json')) {
+                try {
+                    data = JSON.parse(event.target.result);
+                    dashboard.innerHTML = `
+                    <div style="display:flex; gap:24px; align-items: flex-start">
+                    <canvas id ="revenueBarChart" style="width:50%; height:50%;max-width:50%; "></canvas>
+                    <canvas id ="revenuePieChart" style="width:700px; height:700px;max-width:50%; "></canvas>
+                    </div > `;
+
+                    const region = data.map(item => item.region);
+                    const revenue = data.map(item => item.revenue);
+                    const transaction = data.map(item => item.transactions);
+                    const avgOrderValue = data.map(item => item.avgOrderValue);
+
+                    const barCtx = document.getElementById('revenueBarChart').getContext('2d');
+
+                    const pieCtx = document.getElementById('revenueBarChart').getContext('2d');
+
+                    new Chart(barCtx, {
+                        type: 'bar',
+                        data: {
+                            labels: region,
+                            datasets: [{
+                                label: 'Revenue',
+                                data: revenue,
+                                backgroundColor: 'rgba(38,220,105,0.7)'
+                            }, {
+                                label: 'Transaction',
+                                data: transaction,
+                                backgroundColor: 'rgba(69, 85, 227, 0.7)'
+                            },
+                            {
+                                label: 'AvgOrderValue',
+                                data: avgOrderValue,
+                                backgroundColor: 'rgba(238,73,92,0.7)'
+                            }
+                            ]
+                        },
+                        options: {
+                            responsive: true,
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Grouped Bar: Revenue, Transaction, Avg Order Value'
+                                }
+                            },
+                            animation: {
+                                duration: 1000,
+                                easing: 'easeInBounce'
+                            }
+                        }
+                    });
+                }
+                catch (e) {
+                    dashboard.innerHTML = '<p style="color:red;">Invalid JSON file.</p>';
+                }
+            } else {
+                dashboard.innerHTML = '<p style="color:red;">Unsupported file type.</p>';
+            }
+        };
+        reader.readAsText(selectedFile);
+    });
+});
